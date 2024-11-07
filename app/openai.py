e36d31a95e6d1c0cff36f1e0356adaaf8fa7278b
Alex <bocandzo@mail.ru>
2024-11-07 21:15:54 +0300

    Added proxy

--- app/openai.py
+++ app/openai.py
@@ -1,7 +1,12 @@
 from openai import AsyncOpenAI
+import httpx
 from config import AITOKEN
 
-client = AsyncOpenAI(api_key=AITOKEN)
+client = AsyncOpenAI(
+    api_key=AITOKEN,
+    http_client=httpx.AsyncClient(
+        proxies='http://login:pswrd@77.98.18.107:6485',
+        transport=httpx.HTTPTransport(local_address='0.0.0.0')))
 
 
 async def gpt_text(req, model):
